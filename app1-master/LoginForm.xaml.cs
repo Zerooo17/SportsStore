a55434a0998b4a8d02d97c00b2befa95248c7999
Zerooo17 <bashkatov.vityusha@list.ru>
2026-02-26 14:06:37 +0300

    Add files via upload

---
+++ app1-master/LoginForm.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace SportsStoreApp
+{
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
